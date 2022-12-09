d46d4f9632153b2ed36013c3bff113d1cdb69b6e
RashadGr <rashad.tgr@gmail.com>
2022-12-10 03:33:00 +0400

    optimisation for server, client

--- code/client.py
+++ code/client.py
@@ -1,27 +1,36 @@
-# Create client
+# Setup the Client socket
 
 import socket
 
-Port = 17000
-Server = socket.gethostbyname(socket.gethostname())
-addr = (Server, Port)
+
+# Parameters for configuring the 'Client' socket
+# getting local machine name
+HOST = socket.gethostbyname(socket.gethostname())
+# reserving a free port for network
+PORT = 17000    
+addr = (HOST, PORT)
+# setting byte limit for send/receive data
 header = 64
-conn_status = "..!TERMINATED"
+# message to handle disconnections
+connect_status = "..!TERMINATED"
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(addr)
+# Create a client socket and bind it to the address
+clnt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clnt.connect(addr)
 
-# Defining function to send info
-def send(msg):
-    message = msg.encode("utf-8")
-    msg_length = len(message)
-    send_length = str(msg_length).encode("utf-8")
+
+def send(data):
+    """Send serialised data within the allotted bytes."""
+
+    data = data.encode("utf-8")
+    data_size = len(data)
+    send_length = str(data_size).encode("utf-8")
     send_length += b' ' * (header - len(send_length))
-    client.send(send_length)
-    client.send(message)
+    clnt.send(send_length)
+    clnt.send(data)
     
     # Receive delivery message
-    print(client.recv(2048).decode("utf-8"))
+    print(clnt.recv(1024).decode("utf-8"))
 
 send('Hello!')
 input()
@@ -29,25 +38,5 @@ send('How is it going?')
 input()
 send('What is the project progress?')
 
-send(conn_status)
-
-
-
-
-
-
-
-
-# # Create socket for the client
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# # Connect method for client to connect the server
-# s.connect((socket.gethostname(), 1700))
-
-# complete_info = ''
-# while True:
-#     msg = s.recv(7)
-#     if len(msg) <= 0:
-#         break
-#     complete_info += msg.decode("utf-8")
-# print(complete_info)
+# Disconnect at the end of communication
+send(connect_status)
--- code/server.py
+++ code/server.py
@@ -1,72 +1,64 @@
-# Creating server
+# Setup the Server socket
 
 import socket
 
 import threading
 
-Port = 17000
-Server = socket.gethostbyname(socket.gethostname())
-addr = (Server, Port)
+
+# Parameters for configuring the 'Server' socket
+# getting local machine name
+HOST = socket.gethostbyname(socket.gethostname())
+# reserving a free port for network
+PORT = 17000    
+addr = (HOST, PORT)
+# setting byte limit for send/receive data
 header = 64
-conn_status = "..!TERMINATED"
+# message to handle disconnections
+connect_status = "..!TERMINATED"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create a server socket and bind it to the address
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind(addr)
 
-# Binding socket to the address
-server.bind(addr)
 
-# Manage the connection between server and client
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION].. connection to {addr} successful")
-
+def connect(conn, addr):
+    """Manage connections between server and client."""
+    
+    # Notification on successful connections
+    print(f"_CONNECTED_ client addr: {addr} ")
+    
+    # Receive deserialised data within the allotted bytes
     connected = True
     while connected:
-        msg_length = conn.recv(header).decode("utf-8")
-        if msg_length: 
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode("utf-8")
+        data_size = conn.recv(header).decode("utf-8")
+        if data_size: 
+            data_size = int(data_size)
+            data = conn.recv(data_size).decode("utf-8")
             
-            # To handle client disconnection 
-            if msg == conn_status:
+            # Handle client disconnection 
+            if data == connect_status:
                 connected = False
-
-            print(f"[{addr}] {msg}")
+            print(f"[{addr}] {data}")
 
             # Delivery message to client
             conn.send("Delivered!".encode("utf-8"))
-        
     conn.close()
 
 
-def start():
-    """For server to start listening for connections and 
-    pass it to 'handle client' function for processing.
+def listen():
+    """Start listening for connections and pass it to 
+    'connect' function for processing.
     """
-    server.listen()
-    print(f"[LISTENING] Server is listening on {Server}")
+    serv.listen(5)
+    print(f"[LISTENING..] host: {HOST}")
     while True:
-        conn, addr = server.accept()
+        conn, addr = serv.accept()
 
         # To keep track of current users connected
-        # !This method works with Python 3 versions
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        # !This method works with Python 3 versions only
+        thread = threading.Thread(target=connect, args=(conn, addr))
         thread.start()
-        print(f"CLIENTS:_ {threading.active_count()- 1}")
+        print(f"active clients: _ {threading.active_count()- 1}")
 
-start()
-
-
-# # Create socket for the server
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# # Using the method for server/client are on the same machine
-# s.bind((socket.gethostname(), 1700))
-
-# # Create listen method
-# s.listen(5)
-
-# while True:
-#     clt, adr = s.accept()
-#     print(f"Connection to {adr} established")
-#     clt.send(bytes("Client-Server network programming in Python", "utf-8"))
-#     clt.close()
+# Start listening
+listen()
