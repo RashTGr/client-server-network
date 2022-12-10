d25bc70ac025c619764a9953358bcaa115cef357
RashadGr <rashad.tgr@gmail.com>
2022-12-11 03:23:04 +0400

    gitignore updated with dat, json, xml

---
+++ code/pickling.py
@@ -0,0 +1,43 @@
+
+from dictionary import tech_acronyms
+
+a = tech_acronyms
+print(a)
+# import os
+
+# print(os.environ['PYTHONPATH_s'])
+
+
+# The file directory path to enable import on any OS
+
+# print(a)
+# Simple pickle process for binary
+# creates binary file in static folder and writes serialised 
+# data to it
+# numbers = [8, 6, 7, 5, 3, 0, 9]
+# sounds = ('meow', 'woof', 'hi')
+# tech_dict
+# print(tech_dict)
+
+
+# bnry = open('../static/serialise.dat', 'wb')
+# """ Create 'dat' file in static folder to write 
+# serialised binary"""
+
+
+# pickle.dump(numbers, binary)
+# pickle.dump(sounds, binary)
+# pickle.dump(dict_tech, binary)
+
+# binary.close()
+
+
+
+# # Deserializing - Unpickle
+# binary = open('../static/mydata.dat', 'rb')
+
+# deserialised = pickle.load(binary)
+
+# binary.close()
+
+# print(deserialised)
