68e68d0d1ce375ffe7224aa727e47305ed73eb14
RashadGr <rashad.tgr@gmail.com>
2022-12-11 17:24:07 +0400

    serialisation is ready without exception hndlng

--- code/pickling.py
+++ code/pickling.py
@@ -1,43 +1,91 @@
 
-from dictionary import tech_acronyms
+import pickle
 
-a = tech_acronyms
-print(a)
-# import os
+import json
+ 
+from dicttoxml import dicttoxml
 
-# print(os.environ['PYTHONPATH_s'])
+import xmltodict
 
-
-# The file directory path to enable import on any OS
-
-# print(a)
-# Simple pickle process for binary
-# creates binary file in static folder and writes serialised 
-# data to it
-# numbers = [8, 6, 7, 5, 3, 0, 9]
-# sounds = ('meow', 'woof', 'hi')
-# tech_dict
-# print(tech_dict)
+import pprint
 
 
-# bnry = open('../static/serialise.dat', 'wb')
-# """ Create 'dat' file in static folder to write 
-# serialised binary"""
+# Dictionary
+tech_acronyms = dict(zip(['GHz', 'SSH', 'IG', 'IoT', 'M2M', 'S3', 'EC2'], 
+                         ['Giga Hertz', 
+                         'Secure Shell',
+                         'Internet Gateway',
+                         'Internet of Things', 
+                         'Machine to Machine', 
+                         'Simple Storage System',
+                         'Elastic Compute Cloud', 
+                         ]))
 
 
-# pickle.dump(numbers, binary)
-# pickle.dump(sounds, binary)
-# pickle.dump(dict_tech, binary)
+# Serialise data in 'binary' format
+def ser_bnr():
+    with open('../static/serdes.dat', 'wb') as write_bnr:
+        """Create 'dat' file in static folder and write 
+        data in 'binary' serialisation format.
+        """
+        pickle.dump(tech_acronyms, write_bnr)
+    write_bnr.close()
 
-# binary.close()
+# Deserialize 'binary' data
+def des_bnr():
+    with open('../static/serdes.dat', 'rb') as read_bnr:
+            reached_to_end = False
+            while not reached_to_end:
+                try: 
+                    pprint.pprint(pickle.load(read_bnr))
+                except EOFError:
+                    reached_to_end = True
+            read_bnr.close()
+
+
+# Serialise data in 'json' format
+def ser_json():
+    with open('../static/serdes.json', 'w') as write_j:
+        """Create 'json' file in static folder and write 
+        data in 'json' serialisation format.
+        """
+        json.dump(tech_acronyms, write_j)
+    write_j.close()
+
+# Deserialize 'json' data
+def des_json():
+    with open('../static/serdes.json', 'r') as read_j:
+        # Pretty print method to print dict in a well-formatted way
+        pprint.pprint(json.load(read_j))
+    read_j.close()
+        
+
+# Serialise data in 'XML' format
+def ser_xml():
+    with open('../static/serdes.xml', 'w') as write_xml:
+        """Create 'xml' file in static folder and write 
+        data in 'xml' serialisation format.
+        """
+        decode = dicttoxml(tech_acronyms).decode()
+        write_xml.write(decode)
+    write_xml.close()
+
+# Deserialize 'XML' data
+def des_xml():
+    with open('../static/serdes.xml', 'r', encoding='utf-8') as read_xml:
+        my_xml = read_xml.read()
+    # Parse and convert it into dictionary
+    xml_to_dict = xmltodict.parse(my_xml)
+    # Pretty print method to print dict in a well-formatted way
+    pprint.pprint(xml_to_dict)
 
 
 
-# # Deserializing - Unpickle
-# binary = open('../static/mydata.dat', 'rb')
-
-# deserialised = pickle.load(binary)
-
-# binary.close()
-
-# print(deserialised)
+if __name__ == "__main__":
+    # Function calls
+    ser_bnr()
+    des_bnr()
+    # ser_json()
+    # des_json()
+    # ser_xml()
+    # des_xml()
