6ad0c045b681b8047f06a0d295a3a7fc54d56c11
RashadGr <rashad.tgr@gmail.com>
2022-12-16 05:54:17 +0400

    validate encryption file prior to push

---
+++ build/encrypt_fn.py
@@ -0,0 +1,83 @@
+# This script is to encrypt and decrypt a text file"
+
+from binascii import Error
+
+import cryptography
+from cryptography.fernet import Fernet
+
+
+def write_key():
+    """Function to generate and save a key"""
+    key = Fernet.generate_key()
+    with open("../static/crypto.key", "wb") as crypto_file:
+        crypto_file.write(key)
+    crypto_file.close()
+
+def load_key():
+    """The function to call the saved key from the file."""
+    return open("../static/crypto.key", "rb").read()
+
+# Generate and save the key into a file
+# write_key()
+"""Uncomment to renew the saved key, and comment 
+again before decrypt operation.
+"""
+# Initialize the saved key with the Fernet class
+key = load_key()
+
+
+def encrypt(filename, key):
+    """Function to encrypt and write a file"""
+    f = Fernet(key)
+    
+    # Read original data from the file
+    with open(filename, "rb") as file:
+        data = file.read()
+    encrypted_data = f.encrypt(data)
+    file.close()
+
+    # Write encrypted data into the file
+    with open(filename, "wb") as encrypted_file:
+            encrypted_file.write(encrypted_data)
+            """Writing back the encrypted data into the file,
+            this overwrites the original file.
+            """
+    print("Encryption completed!")
+    encrypted_file.close()
+
+
+
+def decrypt(filename, key):
+    """Function to decrypt and write the encrypted file."""
+    f = Fernet(key)
+
+    # Read encrypted data
+    with open(filename, "rb") as encrypt_file:
+        encrypted_data = encrypt_file.read() 
+    try:
+        # Decrypt data
+        decrypted_data = f.decrypt(encrypted_data)
+    except cryptography.fernet.InvalidToken:
+        print("The 'write_key' is likely left in active mode, " 
+              "and a new key generated during decryption.")
+    except Error:
+        print("It is 'incorrect padding' error!")
+    except (TypeError, UnboundLocalError):
+        print("Error has to be fixed.")
+    try:
+        # Write decrypted/original data into the file
+        with open(filename, "wb") as decrypt_file:
+            decrypt_file.write(decrypted_data)
+    except UnboundLocalError:
+        print("Don't run the script. The file might already be corrupted.")
+    else:
+        print("The data is decrypted successfully!")
+    
+    encrypt_file.close()
+    decrypt_file.close()
+    
+file = "../static/demo_text.txt"
+
+if __name__ == "__main__":
+    encrypt(file, key)
+    # decrypt(file, key)
