ef853dc46667ed505e6f53904aed3654fdf64c4d
sebakoke92 <116904089+sebakoke92@users.noreply.github.com>
2022-12-17 07:24:06 +0300

    Update test_encrypt.py
    unit test for decrypt & encrypt file

--- build/test_encrypt.py
+++ build/test_encrypt.py
@@ -1,51 +1,47 @@
 """This is unit test for text file Encryption 
 and Decryption.
 """
-
+    
 import unittest
+import os
+from binascii import Error
+import cryptography
+from cryptography.fernet import Fernet
 
-import encrypt_fn as enc
-
-
-class PicklingTest(unittest.TestCase):
-    # configure input & output to pass to each test scenario
+class TestEncryptDecrypt(unittest.TestCase):
     def setUp(self):
-        self.input_enc = file
-        self.output_dec = file2
- 
-    # cleanup generated data during test cases
-    def tearDown(self):
-        self.input_enc = 0
-        self.output_dec = 0
-        
-    # Unit test to test encryption and decryption script
-    def test_encrypt(self):
-        self.assertEqual(enc.encrypt(self.input_enc, test_key), 
-                         enc.decrypt(self.output_dec, test_key))
+        self.key_path = "D:\zip file version\client-server-network-combatzone-2\static\crypto.key"
+        self.file_path = "D:\zip file version\client-server-network-combatzone-2\static\serdes.dat"
 
-    def test_encrypt_2(self):
-        self.assertEqual(enc.encrypt(self.input_enc, test_key_2), 
-                         enc.decrypt(self.output_dec, test_key_2))
+        # Generate and save a key
+        key = Fernet.generate_key()
+        with open(self.key_path, "wb") as crypto_file:
+            crypto_file.write(key)
 
-    def test_encrypt_3(self):
-        self.assertEqual(enc.encrypt(self.input_enc, test_key_3), 
-                         enc.decrypt(self.output_dec, test_key_3))
+    def test_encrypt_decrypt(self):
+        # Load the key
+        key = open(self.key_path, "rb").read()
 
-    def test_encrypt_4(self):
-        self.assertEqual(enc.encrypt(self.input_enc, test_key_4), 
-                         enc.decrypt(self.output_dec, test_key_4))
+        # Encrypt the file
+        f = Fernet(key)
+        with open(self.file_path, "rb") as file:
+            data = file.read()
+        encrypted_data = f.encrypt(data)
+        with open(self.file_path, "wb") as encrypted_file:
+            encrypted_file.write(encrypted_data)
 
-  
-# File path and keys to be used in different test cases
-file = "../static/demo_text.txt"
-file2 = "../static/demo_text.txt"
-test_key = "LoqtVWM5aPJOTmKx8IA-MPKgkQpbPDob5BK9aVR5aE0="
-test_key_2 = "1NaJ_rQ5Q5v_5Zi3q3UXjIEBoDL2lwdG_HwurlmUWV4="
-test_key_3 = "78LEjqropNkHZI0eBC1HqXyWJwMHIzvHkVv4E0mlw4U="
-test_key_4 = "E2e71siE2xdPP9Zj3XOPFj8mHNDmDzKcPYcfm1tQbXA="
+        # Decrypt the file
+        f = Fernet(key)
+        with open(self.file_path, "rb") as encrypt_file:
+            encrypted_data = encrypt_file.read()
+        decrypted_data = f.decrypt(encrypted_data)
 
-print("\nUnit tests for text file Encryption is completed!\n")
+        # Check that the decrypted data is the same as the original data
+        with open(self.file_path, "rb") as file:
+            original_data = file.read()
+        self.assertEqual(decrypted_data, original_data)
 
+   
 
 if __name__ == "__main__":
     unittest.main()
